001cd606e10de4552b0676977f20debab8b93419
Leonardo-Messias <leonardo.messias@hotmail.com>
2022-01-30 06:09:34 -0300

    ATT Commit

--- src/one/digital/innovation/gof/Test.java
+++ src/one/digital/innovation/gof/Test.java
@@ -1,7 +1,12 @@
 package one.digital.innovation.gof;
 
+import one.digital.innovation.gof.singleton.SingletonEager;
+import one.digital.innovation.gof.singleton.SingletonLazy;
+import one.digital.innovation.gof.strategy.*;
+
 public class Test {
     public static void main(String[] args) {
+        // Singleton
         SingletonLazy lazy = SingletonLazy.getInstance();
         System.out.println(lazy);
         lazy = SingletonLazy.getInstance();
@@ -11,6 +16,17 @@ public class Test {
         System.out.println(eager);
         eager = SingletonEager.getInstance();
         System.out.println(eager);
+
+        //Strategy
+
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
 
     }
 }
